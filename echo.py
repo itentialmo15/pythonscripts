bc18d1b71355892e5f7065970a93fd182c417a81
itentialmo15 <mohan.rajanna@itential.com>
2026-02-06 23:21:59 -0500

    Implement argparse for command line input
    Refactor echo.py to use argparse for command input.

--- echo.py
+++ echo.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import sys
 import argparse
+def main():
+    parser.add_argument('--command', required=True, help="Command to run")
+    args = parser.parse_args()
+    output =args.command
+    print(output)
+    
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python echo.py <text>")
-        sys.exit(1)
-
-    text = parser.add_argument('--command', required=True, help="Command to run")
-    print(text)
-
+    main()
