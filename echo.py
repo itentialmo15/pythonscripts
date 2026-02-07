66698d0f531043d9c8425f02206d31f84a81b38e
itentialmo15 <mohan.rajanna@itential.com>
2026-02-06 23:27:22 -0500

    Add argparse for command line argument parsing

--- echo.py
+++ echo.py
@@ -2,10 +2,11 @@
 import sys
 import argparse
 def main():
+    parser = argparse.ArgumentParser(description="test")
     parser.add_argument('--command', required=True, help="Command to run")
     args = parser.parse_args()
     output =args.command
     print(output)
-    
+
 if __name__ == "__main__":
     main()
